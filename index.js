0c2f2c9e13fa7749cb16928f5e1831792704fb72
Vicente Barra <vicente.barra.94@gmail.com>
2022-06-30 01:38:14 -0400

    Path * added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const { dbConnection } = require('./database/config')
 require('dotenv').config()
 
@@ -21,6 +22,10 @@ app.use(express.json())
 // Rutas
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
